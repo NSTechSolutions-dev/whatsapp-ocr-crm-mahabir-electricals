67dc6a46dfb8ba5dad0cbedc60b2f9f19f20729b
thebluepeakstudio <thebluepeakstudio@gmail.com>
2026-07-16 16:51:01 +0530

    Quote Combine

---
+++ Whatsapp-OCR-CRM/frontend/src/components/ui/tabs.d.ts
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+export declare const Tabs: React.FC<any>;
+export declare const TabsList: React.ForwardRefExoticComponent<any>;
+export declare const TabsTrigger: React.ForwardRefExoticComponent<any>;
+export declare const TabsContent: React.ForwardRefExoticComponent<any>;
